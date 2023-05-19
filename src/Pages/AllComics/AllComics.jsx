3962d624fb00e32adf921fff3b8c7e74fb85a62d
joySutradhor <joysutradhorcmt@gmail.com>
2023-05-20 00:41:30 +0600

    all comics book are updated

--- src/Pages/AllComics/AllComics.jsx
+++ src/Pages/AllComics/AllComics.jsx
@@ -29,31 +29,23 @@ const AllComics = () => {
                 </div>
 
             </form>
-            <div className="overflow-x-auto w-full">
+            <div className="overflow-x-auto w-full px-10">
                 <table className="table w-full">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th>
-
-                            </th>
                             <th>Seller Name</th>
                             <th>Comic Name</th>
                             <th>category Name</th>
                             <th>Price</th>
                             <th>Quantity</th>
-                            <th></th>
+                            <th>Show Details</th>
                         </tr>
                     </thead>
 
                     {
                         comics.map(comic => <tbody key={comic._id}>
                             <tr>
-                                <th>
-                                    <button className="btn btn-circle btn-outline">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                    </button>
-                                </th>
                                 <td>
                                     <div className="flex items-center space-x-3">
                                         <div>
--- src/Pages/AllComics/Details.jsx
+++ src/Pages/AllComics/Details.jsx
@@ -3,20 +3,27 @@ import { useLoaderData } from 'react-router-dom';
 
 const Details = () => {
     const details = useLoaderData();
-    const { url } = details;
+    const { cetegory ,sellerName ,  description , email , quantity , price , url , name , ratting } = details;
     console.log(details)
     return (
-        <div className="card lg:card-side bg-base-100 mx-10 py-12">
-            <figure><img src={url} alt="Album" /></figure>
+        <div className="card lg:card-side bg-base-100 mx-[10%] py-12 shadow-sm">
+            <div>
+            <figure><img src={url} alt="Album" className='rounded-md h-[400px] w-[400px]'/></figure>
+            </div>
             <div className="card-body">
-                <h2 className="card-title">New album is released!</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Listen</button>
+                <h2 className="card-title">{cetegory}</h2>
+                <div className='text-gray-500'>
+                <p>Seller : {sellerName}</p>
+                <p>Comic Name : {name}</p>
+                <p>Price : {price}</p>
+                <p>Quantity : {quantity}</p>
+                <p>Email : {email}</p>
+                <p>Description : {description}</p>
+                <p>Ratting : {ratting}</p>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Details; <h1>details page</h1>
+export default Details; 
