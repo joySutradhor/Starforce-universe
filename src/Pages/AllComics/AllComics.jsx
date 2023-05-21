3334a0ac7799ffabf3c9f04d705d0dd73216fd02
joySutradhor <joysutradhorcmt@gmail.com>
2023-05-21 20:32:14 +0600

    search feild added

--- src/Pages/AllComics/AllComics.jsx
+++ src/Pages/AllComics/AllComics.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import UseTitle from '../../Shared/UseTitle';
 
 
 
 const AllComics = () => {
-    const comics = useLoaderData();
+    const allcomics = useLoaderData();
+    console.log(allcomics)
     UseTitle("All Comics")
-
+    const [comics, setComics] = useState([])
+    useEffect(() => {
+        setComics(allcomics)
+    } , [])
+    const handleSearch = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const data = form.search.value;
+        console.log(data)
+        fetch(`https://starforce-universe-server.vercel.app/search/${data}`)
+            .then(res => res.json())
+            .then(data => setComics(data))
+    }
 
     return (
         <div>
-            <form className='flex justify-end  items-center mx-5 py-2 gap-5'> <input type="text" placeholder="Ensure comic name is matched" name='search' className="input input-bordered input-accent w-full max-w-xs" />
+            <form onSubmit={handleSearch} className='flex justify-end  items-center mx-5 py-2 gap-5'> <input type="text" placeholder="Ensure comic name is matched" name='search' className="input input-bordered input-accent w-full max-w-xs" />
                 <div>
                     <button className="btn btn-outline btn-accent">Search</button>
                 </div>
@@ -32,7 +45,7 @@ const AllComics = () => {
                     </thead>
 
                     {
-                        comics.map(comic => <tbody key={comic._id}>
+                        comics?.map(comic => <tbody key={comic._id}>
                             <tr>
                                 <td>
                                     <div className="flex items-center space-x-3">
